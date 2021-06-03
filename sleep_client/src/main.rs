d49fa83e1a6471e5e3e23d2c6a1828218b0b675e
kozmod <kozmo23g@gmail.com>
2021-06-03 21:29:27 +0300

    change sleep-client to async

--- sleep_client/src/main.rs
+++ sleep_client/src/main.rs
@@ -2,32 +2,40 @@ extern crate chrono;
 
 use chrono::offset::Utc;
 use chrono::DateTime;
-use std::time::SystemTime;
+use std::time::{SystemTime};
 
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use async_std::io::prelude::*;
+use async_std::net::{TcpListener, TcpStream};
+use async_std::task::spawn;
+use futures::StreamExt;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+#[async_std::main]
+async fn main() {
+    let port = ":8080";
+    println!("Server started on port {}", port);
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
+    let listener = TcpListener::bind(["127.0.0.1", port].join("")).await.unwrap();
+    listener
+        .incoming()
+        .for_each_concurrent(/* limit */ None, |stream| async move {
+            let stream = stream.unwrap();
+            spawn(handle_connection(stream));
+        })
+        .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
+
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
     let header = String::from("HTTP/1.1 200 OK\r\n\r\n");
     let time: DateTime<Utc> = SystemTime::now().into();
     let response = format!("{}{:?}", header, time);
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
 
